30bd0928f1106f2b1e16e068a3cbd1e191e484f9
Cristian029 <cristiavaet@gmail.com>
2022-05-12 23:43:21 -0300

    Changes by gabri

--- src/Cordenada.java
+++ src/Cordenada.java
@@ -25,5 +25,66 @@ public class Cordenada {
                 ", y=" + this.y +
                 '}';
     }
+
+    /*
+    public boolean equals (Object obj)
+    {
+        if(this==obj)
+            return true;
+
+        if(obj==null)
+            return false;
+
+        if(this.getClass()!=obj.getClass())
+            return false;
+
+        Cordenada cord = (Cordenada) obj;
+
+        if(this.x!= cord.x)
+            return false;
+
+        return this.y == cord.y;
+
+    }
+    */
+    /*
+    public int hashCode ()
+    {
+        ret = ret*7 + Integer.valueOf(this.x).hashCode();
+        ret = ret*7 + Integer.valueOf(this.y).hashCode();
+
+        if (ret<0)
+            ret=-ret;
+
+        return ret;
+    }
+    */
+    /*
+    // construtor de copia
+    public Cordenada (Cordenada modelo) throws Exception
+    {
+        if(modelo == null)
+            throw new Exception("Modelo ausente");
+
+        this.x = modelo.x;
+        this.y = modelo.y;
+    }
+    */
+
+    /*
+    public Object clone ()
+    {
+        Cordenada ret=null;
+
+        try
+        {
+            ret = new Cordenada(this);
+        }
+        catch(Exception erro)
+        {}
+
+        return ret;
+    }
+    */
 }
 
